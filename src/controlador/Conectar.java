ac66f9af3270bd015de60dc3fb6a50c51b9046a5
lab7 <lab7@LAB07M06.fatecrl.com>
2017-03-29 20:24:56 -0300

    Remoção de libs desnecessárias.

--- src/controlador/Conectar.java
+++ src/controlador/Conectar.java
@@ -11,6 +11,7 @@ public class Conectar {
     final static String URL = "jdbc:mysql://localhost:3306/crud_java";
     final static String USER = "root";
     final static String PASS = "root";
+    
     public static Connection Banco() throws SQLException
     {
         Connection Con = (Connection)DriverManager.getConnection(URL,USER,PASS);
